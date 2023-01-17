cd99acdffbad08ea1a9770e45f0c372ae99ccbf5
Ludwig Schlame <ludwig.schlame@gmail.com>
2023-01-17 21:08:08 +0100

    fix: command parsing issue when empty command was received. Refs: #1

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,13 @@ fn main() {
         while let Some(command) = commands.next() {
             // everything after the first whitespace character is interpreted as args to the command
             let mut parts = command.split_whitespace();
-            let command = parts.next().unwrap();
+
+            // in case the command is an empty string, split_whitespace returns an empty iterator and next returns 'None'
+            let command = match parts.next() {
+                Some(command) => command,
+                None => continue,
+            };
+
             let args = parts;
 
             match command {
